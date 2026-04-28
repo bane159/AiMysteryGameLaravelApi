cbeaa69ecce87f0325f12767bf0da37bd85fbdf2
Branislav <branislavr03@gmail.com>
2026-04-28 21:29:29 +0200

    Add default attributes for xp and level in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,11 @@ class User extends Authenticatable implements JWTSubject
         'level',
     ];
 
+    protected $attributes = [
+        'xp'    => 0,
+        'level' => 1,
+    ];
+
     /**
      * The attributes that should be hidden for serialization.
      *
